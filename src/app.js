2dcf4fb3f3eb189d6f3e52bc25a2e267a2a3afae
Your Name <you@example.com>
2018-02-14 10:08:55 +0000

    2

--- src/app.js
+++ src/app.js
@@ -1,18 +1,66 @@
 const openStream = require('./openStream');
 const $ = require('jquery');
-const Peer = require('simple-peer');
 const playVideo = require('./playVideo');
 
-openStream(function (stream){
-	playVideo(stream,'localstream');
-	const p = new Peer({initiator: location.hash === '#1', trickle: false, stream: stream});
-	p.on('signal', token => {
-		$('#txtMySignal').val(JSON.stringify(token));
+const uid = require('uid');
+const Peer = require('peerjs');
+
+const io = require('socket.io-client');
+
+const socket  = io('127.0.0.1:8081');
+const getIceObject = require('./getIceObject');
+
+// getIceObject(iceConfig =>{
+	const connectionObj = {
+		host: 'seanpeer.herokuapp.com',
+		port: '443',
+		secure: true,
+		key: 'peerjs',
+		// config: iceConfig
+	};
+	const peerId = getPeer();
+	socket.emit('NEW_PEER_ID', peerId);
+	const peer = Peer(peerId, connectionObj);
+	// console.log('111');
+	// console.log(peer);
+	$('#ulPeerId').on('click', 'li', function (){
+		const peerId = $(this).text();
+		console.log(peerId);
+		openStream(stream => {
+			playVideo(stream, 'localstream');
+			const call = peer.call(peerId, stream);
+			console.log(call);
+			call.on('stream', remoteStream => playVideo(remoteStream, 'friendstream'));
+		})
 	});
-	$('#btnConnect').click(() => {
-		const friendSignal = JSON.parse($('#txtFSignal').val());
-		p.signal(friendSignal);
+	peer.on('call', call => {
+		// console.log('111');
+		openStream(stream => {
+			playVideo(stream, 'localstream');
+			call.answer(stream);
+			call.on('stream', remoteStream => playVideo(remoteStream, 'friendstream'));
+		});
 	});
-	p.on('stream', friendStream => playVideo(friendStream, 'friendstream'))
+// })
+
+function getPeer(){
+	const id = uid(10);
+	$('#peer-id').append(id);
+	return id;
+}
+
+socket.on('ONLINE_PEER', arrPeerId =>{
+	// console.log(arrPeerId);
+	arrPeerId.forEach( id => {
+		$('#ulPeerId').append('<li id='+id+'>'+id+'</li>');
+	})
+
 });
+
+socket.on('SOME_ONE_DISCONNECT', peerid => {
+	$('#'+peerid).remove();
+});
+
+socket.on('NEW_CLIENT_CONNECT', id => $('#ulPeerId').append('<li id='+id+'>'+id+'</li>'));
+
 
